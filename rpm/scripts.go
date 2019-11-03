4418b5ec470e683d7dc46d778b9291bbec58445a
Andrey Tuzhilin <andrey@3adigital.ru>
2019-11-03 21:07:59 +0300

    fix: add user script

--- rpm/scripts.go
+++ rpm/scripts.go
@@ -3,7 +3,7 @@ package rpm
 const scriptCreateUser = `
 getent group %{package_user} > /dev/null || groupadd -r %{package_user}
 getent passwd %{package_user} > /dev/null || \
-    useradd -r -d %{_localstatedir}/lib/%{package_user} -g %{package_user} \
+    useradd -r -d /var/lib/%{package_user} -g %{package_user} \
     -s /sbin/nologin %{package_user}
 exit 0
 `
