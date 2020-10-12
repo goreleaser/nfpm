7e14c811128d8021fdd3e7c985d84459b0b0ee31
Benno Lin <blueworrybear@gmail.com>
2020-10-12 23:04:25 +0800

    test: Add APK and RSA sign test
    To increase test coverage.
    
    See #229

--- internal/sign/rsa_test.go
+++ internal/sign/rsa_test.go
@@ -2,8 +2,19 @@ package sign
 
 import (
 	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha1"
+	"crypto/x509"
+	"encoding/asn1"
+	"encoding/pem"
+	"io/ioutil"
+	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -48,4 +59,50 @@ func TestInvalidHash(t *testing.T) {
 	invalidDigest := []byte("test")
 	_, err := RSASignSHA1Digest(invalidDigest, "testdata/rsa.priv", "hunter2")
 	require.EqualError(t, err, "digest is not a SHA1 hash")
+}
+
+func TestRSAVerifySHA1DigestError(t *testing.T) {
+	rsaKey, err := rsa.GenerateKey(rand.Reader, 12)
+	if err != nil {
+		t.Fatal(err)
+	}
+	asn1Bytes, err := asn1.Marshal(rsaKey.PublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ecdsaKey, err := ecdsa.GenerateKey(elliptic.P521(), rand.Reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	x509Bytes, err := x509.MarshalPKIXPublicKey(&ecdsaKey.PublicKey)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pemKeys := []*pem.Block{
+		{
+			Type:  "PUBLIC KEY",
+			Bytes: asn1Bytes,
+		},
+		{
+			Type:  "PUBLIC KEY",
+			Bytes: x509Bytes,
+		},
+	}
+
+	for _, pemKey := range pemKeys {
+		cwd, _ := os.Getwd()
+
+		keyFile, err := ioutil.TempFile(cwd, "*.pem")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.Remove(keyFile.Name())
+		pem.Encode(keyFile, pemKey)
+		keyFile.Close()
+		digest := sha1.New().Sum(nil)
+		assert.Error(t, RSAVerifySHA1Digest(digest, []byte{}, keyFile.Name()))
+	}
 }
