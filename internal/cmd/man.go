13e76671c005e1da6d689ffb5e5e0d118c546f85
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2022-02-02 23:30:46 -0300

    feat: generate man pages (#443)
    * feat: migrate to coral
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>
    
    * feat: generate man pages
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>

---
+++ internal/cmd/man.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/muesli/coral"
+	mcoral "github.com/muesli/mango-coral"
+	"github.com/muesli/roff"
+)
+
+type manCmd struct {
+	cmd *coral.Command
+}
+
+func newManCmd() *manCmd {
+	root := &manCmd{}
+	cmd := &coral.Command{
+		Use:                   "man",
+		Short:                 "Generates nfpm's command line manpages",
+		SilenceUsage:          true,
+		DisableFlagsInUseLine: true,
+		Hidden:                true,
+		Args:                  coral.NoArgs,
+		RunE: func(cmd *coral.Command, args []string) error {
+			manPage, err := mcoral.NewManPage(1, root.cmd.Root())
+			if err != nil {
+				return err
+			}
+
+			_, err = fmt.Fprint(os.Stdout, manPage.Build(roff.NewDocument()))
+			return err
+		},
+	}
+
+	root.cmd = cmd
+	return root
+}
