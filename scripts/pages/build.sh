023da3d4faf61437f45d4187e30680c7250ed935
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-12-29 11:18:10 -0300

    docs: update pip
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- scripts/pages/build.sh
+++ scripts/pages/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euo pipefail
+pip install -U pip
 pip install -U mkdocs-material mkdocs-minify-plugin lunr
 version="$(curl -sSf -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/goreleaser/nfpm/releases/latest" | jq -r '.tag_name')"
 sed -s'' -i "s/__VERSION__/$version/g" www/docs/install.md
