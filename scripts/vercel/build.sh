c46f14323aa247993a696a56e6c5b039019b4cae
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2022-12-29 21:04:56 -0300

    docs: remove vercel deploy (#595)
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>
    
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- scripts/vercel/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-version="$(curl -sSf -H "Authorization: Bearer $GITHUB_TOKEN" "https://api.github.com/repos/goreleaser/nfpm/releases/latest" | jq -r '.tag_name')"
-sed -s'' -i "s/__VERSION__/$version/g" www/docs/install.md
-mkdocs build -f www/mkdocs.yml
--- scripts/vercel/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-yum install -y jq
-pip install mkdocs-material mkdocs-minify-plugin lunr
