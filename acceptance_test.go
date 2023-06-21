3e892a7551e1c3a92a9e0c25208d46e4a7afa10f
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-06-21 19:11:51 -0300

    test: signatures on centos 7 and 8 (#678)
    * test: signatures on centos 7 and 8
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
    
    * fix: remove test for centos7, added for centos9
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
    
    * chore: add back build tags
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
    
    * chore: revert unneeded changes
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- acceptance_test.go
+++ acceptance_test.go
@@ -259,17 +259,38 @@ func TestDebSpecific(t *testing.T) {
 	}
 }
 
+func TestRpmCentosSign(t *testing.T) {
+	t.Parallel()
+	for _, os := range []string{"centos9", "centos8"} {
+		os := os
+		t.Run(fmt.Sprintf("rpm/signed/amd64/%s", os), func(t *testing.T) {
+			t.Parallel()
+			target := "signed"
+			accept(t, acceptParms{
+				Name:   fmt.Sprintf("sign_%s_amd64", os),
+				Conf:   "core.signed.yaml",
+				Format: "rpm",
+				Docker: dockerParams{
+					File:   fmt.Sprintf("rpm_%s.dockerfile", os),
+					Target: target,
+					Arch:   "amd64",
+				},
+			})
+		})
+	}
+}
+
 func TestDebSign(t *testing.T) {
 	t.Parallel()
 	for _, arch := range formatArchs["deb"] {
 		for _, sigtype := range []string{"dpkg-sig", "debsign"} {
 			func(t *testing.T, testSigtype, testArch string) {
 				t.Run(fmt.Sprintf("%s/%s", testArch, testSigtype), func(t *testing.T) {
+					t.Parallel()
 					target := "signed"
 					if testSigtype == "dpkg-sig" {
 						target = "dpkg-signed"
 					}
-					t.Parallel()
 					if testArch == "ppc64le" && os.Getenv("NO_TEST_PPC64LE") == "true" {
 						t.Skip("ppc64le arch not supported in pipeline")
 					}
