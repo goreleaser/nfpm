f145eba7825a19df7a40f46a2ee1dcde50e2e50e
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2023-06-21 20:04:24 +0000

    chore: revert unneeded changes
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- acceptance_test.go
+++ acceptance_test.go
@@ -284,28 +284,28 @@ func TestDebSign(t *testing.T) {
 	t.Parallel()
 	for _, arch := range formatArchs["deb"] {
 		for _, sigtype := range []string{"dpkg-sig", "debsign"} {
-			testSigtype := sigtype
-			testArch := arch
-			t.Run(fmt.Sprintf("%s/%s", testArch, testSigtype), func(t *testing.T) {
-				t.Parallel()
-				target := "signed"
-				if testSigtype == "dpkg-sig" {
-					target = "dpkg-signed"
-				}
-				if testArch == "ppc64le" && os.Getenv("NO_TEST_PPC64LE") == "true" {
-					t.Skip("ppc64le arch not supported in pipeline")
-				}
-				accept(t, acceptParms{
-					Name:   fmt.Sprintf("%s_sign_%s", testSigtype, testArch),
-					Conf:   fmt.Sprintf("deb.%s.sign.yaml", testSigtype),
-					Format: "deb",
-					Docker: dockerParams{
-						File:   "deb.dockerfile",
-						Target: target,
-						Arch:   testArch,
-					},
+			func(t *testing.T, testSigtype, testArch string) {
+				t.Run(fmt.Sprintf("%s/%s", testArch, testSigtype), func(t *testing.T) {
+					t.Parallel()
+					target := "signed"
+					if testSigtype == "dpkg-sig" {
+						target = "dpkg-signed"
+					}
+					if testArch == "ppc64le" && os.Getenv("NO_TEST_PPC64LE") == "true" {
+						t.Skip("ppc64le arch not supported in pipeline")
+					}
+					accept(t, acceptParms{
+						Name:   fmt.Sprintf("%s_sign_%s", testSigtype, testArch),
+						Conf:   fmt.Sprintf("deb.%s.sign.yaml", testSigtype),
+						Format: "deb",
+						Docker: dockerParams{
+							File:   "deb.dockerfile",
+							Target: target,
+							Arch:   testArch,
+						},
+					})
 				})
-			})
+			}(t, sigtype, arch)
 		}
 	}
 }
